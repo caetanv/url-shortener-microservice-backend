44f7989c1090bb5d25331b147b2f8caecfbd65c4
Kallyne Layane <hellookallyne@gmail.com>
2023-10-27 18:52:19 -0300

    Setup mongoose

--- api/index.js
+++ api/index.js
@@ -2,6 +2,21 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const Schema = mongoose.Schema;
+
+const UrlSchema = new Schema({
+  original_url: String,
+  short_url: Number,
+});
+
+const Url = mongoose.model('Url', UrlSchema);
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -13,15 +28,15 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
+app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
